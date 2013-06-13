643c4b8ce4bc849d0be18602e089de0f7ffc1973
sicentendu <ryancwilliams9@gmail.com>
2013-06-12 20:41:55 -0400

    Shows and users set up, db going strong, good night

---
+++ app/views/shows/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @show, :name, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 NewEngland::Application.routes.draw do
   devise_for :users
+
+  root to: 'shows#index'
+  resources :shows
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -40,7 +44,7 @@ NewEngland::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
